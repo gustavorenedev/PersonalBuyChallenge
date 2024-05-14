d48403b0bc3500e5ade5b65c4986d83f9fc0fe45
Gustavo René <renegustavo42@gmail.com>
2024-05-14 19:34:08 -0300

    Add DbContext, StringConnection e configurações na Program.cs

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalBuy.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> Options) : base(Options)
+        {
+        }
+    }
+}
